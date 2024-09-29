30c432f99b097d6b1b3c128e647ccd0bff6d74c6
Rhodric McCurdy <rmccurdy268@gmail.com>
2024-09-29 01:42:41 -0600

    added(logoutTests)

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -4,14 +4,17 @@ const app = require('../service');
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+  jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
+
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
-  expect(testUserAuthToken).toBeTruthy()
 });
 
-test('login', async () => {
+test('login test user', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
   expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
@@ -20,3 +23,15 @@ test('login', async () => {
   expect(loginRes.body.user).toMatchObject(user);
   expect(password).toBeTruthy();
 });
+
+test('logout test user', async () => {
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+  expect(logoutRes.body.message).toBe("logout successful");
+});
+
+test('logout test user without token', async () => {
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer wrongtoken`);
+  expect(logoutRes.status).toBe(401)
+});
+
+
