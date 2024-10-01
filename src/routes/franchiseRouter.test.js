ee1cbb29f127592ef6d388e06bca627864143662
Rhodric McCurdy <rmccurdy268@gmail.com>
2024-09-30 20:26:00 -0600

    added create franchise test. 51.31% coverage

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -2,8 +2,18 @@ const request = require('supertest');
 const app = require('../service');
 
 const { Role, DB } = require('../database/database.js');
-let admin;
+let admin, adminToken, testUser, testUsertoken;
 
+async function createTestUser() {
+  let user = { password: 'testerPassword', roles: [{ role: Role.Diner }] }
+  user.name = Math.random().toString(36).substring(2, 12);
+  user.email = user.name + '@admin.com';
+
+  await DB.addUser(user);
+
+  user.password = 'toomanysecrets';
+  return user;
+}
 async function createAdminUser() {
   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
   user.name = Math.random().toString(36).substring(2, 12);
@@ -15,9 +25,35 @@ async function createAdminUser() {
   return user;
 }
 
+async function login(user){
+  let loginRes;
+  try{
+    loginRes = await request(app).put('/api/auth').send(user);
+  }
+  catch (error){
+    console.log("login didnt work");
+  }
+  finally{
+    return loginRes.body.token;
+  }
+}
+
 beforeAll(async () => {
   admin = await createAdminUser();
+  testUser = await createAdminUser();
+  adminToken = await login(admin);
+  testUsertoken = await login(testUser);
+
 });
+test("create franchise test", async () =>{
+  expect(true).toBeTruthy()
+  const createRes = await request(app).post('/api/franchise/').set('Authorization', `Bearer ${adminToken}`).send({"name": "otherpizzapocket", "admins": [{"email": admin.email}]})
+  expect(createRes.status).toBe(200);
+});
+
+
+
+
 
 afterAll(async () => {
   const connection = await DB.getConnection();
@@ -28,10 +64,4 @@ afterAll(async () => {
   finally{
     connection.end();
   }
-});
-
-
-
-test("create franchise test", async () =>{
-  
 });
