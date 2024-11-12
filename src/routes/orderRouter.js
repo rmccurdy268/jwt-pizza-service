665338acc9ced8c30d4078e125e2ff78fc9e652c
Rhodric McCurdy <rmccurdy268@gmail.com>
2024-11-12 11:09:12 -0700

    fixed oopsie with authrouter

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -3,7 +3,6 @@ const config = require('../config.js');
 const { Role, DB } = require('../database/database.js');
 const { authRouter } = require('./authRouter.js');
 const { asyncHandler, StatusCodeError } = require('../endpointHelper.js');
-const metrics = require('../metrics.js')
 
 
 const orderRouter = express.Router();
