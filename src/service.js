ebabde73972f26b10a07653ca96af32988c07fce
Rhodric McCurdy <rmccurdy268@gmail.com>
2024-11-15 12:46:14 -0700

    added initial logger

--- src/service.js
+++ src/service.js
@@ -5,7 +5,8 @@ const franchiseRouter = require('./routes/franchiseRouter.js');
 const version = require('./version.json');
 const config = require('./config.js');
 const metrics = require('./metrics.js');
-
+const Logger = require('pizza-logger');
+const logger = new Logger(config);
 
 const app = express();
 app.use(express.json());
